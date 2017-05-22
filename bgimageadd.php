7b0edd12751901657e524d689fca545568b0bc2f
Xiaosea <maysrp@gmail.com>
2017-05-23 00:58:34 +0800

    添加缓存base64

--- bgimageadd.php
+++ bgimageadd.php
@@ -1,18 +1,19 @@
 <?php 
 	$key=isset($_POST['key'])?(int)$_POST['key']:'0';
 	$now=time();
-	$bnow=$now-1800;
-	$anow=$now+1800;
-	if($bnow<$key&&$key<$anow){
-		$imgurl=bgimageurl("./image/");//末尾必须加“/”,相对目录
-		$info=file_get_contents($imgurl);
-		//header("Content-type: image/jpg");
-		echo base64_encode($info);
-	}else{
-		return false;
+	$bnow=$now-43200;
+	$anow=$now+43200;
+	$pg='/domian\.org/';
+	if(preg_match($pg,$_SERVER['HTTP_REFERER'])){
+		if($bnow<$key&&$key<$anow){
+			$imgurl=bgimageurl("./image/");//末尾必须加“/”,相对目录
+			$dir64=base64($imgurl);
+			$info=file_get_contents($dir64);
+			echo $info;
+		}else{
+			return false;
+		}
 	}
-	
-
 	function bgimageurl($dir){
 		$dirarray=scandir($dir);//末尾必须加“/”,相对目录
 		$imgarray=array();
@@ -26,4 +27,12 @@
 		$count=count($imgarray)-1;
 		$mt=mt_rand(0,$count);
 		return $imgarray[$mt];
+	}
+	function base64($dir){
+		if(!is_file($dir.".64")){
+			$info=file_get_contents($dir);
+			$bs=base64_encode($info);
+			file_put_contents($dir.".64", $bs);
+		}
+		return $dir.".64";
 	}
